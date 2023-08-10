fa755a55f3952a250828eae7037ddaf1baaba046
Tanjina Akter Nisa <140683300+tanjinanisa86@users.noreply.github.com>
2023-08-10 22:11:12 +0600

    Add files via upload

---
+++ two.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<n;i++){
+        for(j=1;j<=i;j++){
+            printf("* ",i);
+        }
+        printf("\n");
+    }
+return 0;
+}
+//printing stars
+//sample output
+/*
+* *
+* * *
+* * * *
+* * * * *
